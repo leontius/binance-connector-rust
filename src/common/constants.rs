acc9d5e1902fde62bbf640ca6e1373f2b9b92747
Leon Zhang <leontius.zy@gmail.com>
2025-10-01 17:35:04 +0800

    Update common constants.rs

--- src/common/constants.rs
+++ src/common/constants.rs
@@ -90,11 +90,11 @@ pub const SIMPLE_EARN_REST_API_PROD_URL: &str = "https://api.binance.com";
 
 // Spot constants
 pub const SPOT_REST_API_PROD_URL: &str = "https://api.binance.com";
-pub const SPOT_REST_API_TESTNET_URL: &str = "https://testnet.binance.vision";
+pub const SPOT_REST_API_TESTNET_URL: &str = "https://demo-api.binance.com";
 pub const SPOT_WS_API_PROD_URL: &str = "wss://ws-api.binance.com:443/ws-api/v3";
-pub const SPOT_WS_API_TESTNET_URL: &str = "wss://ws-api.testnet.binance.vision/ws-api/v3";
+pub const SPOT_WS_API_TESTNET_URL: &str = "wss://demo-ws-api.binance.com/ws-api/v3";
 pub const SPOT_WS_STREAMS_PROD_URL: &str = "wss://stream.binance.com:9443";
-pub const SPOT_WS_STREAMS_TESTNET_URL: &str = "wss://stream.testnet.binance.vision";
+pub const SPOT_WS_STREAMS_TESTNET_URL: &str = "wss://demo-stream.binance.com";
 pub const SPOT_REST_API_MARKET_URL: &str = "https://data-api.binance.vision";
 pub const SPOT_WS_STREAMS_MARKET_URL: &str = "wss://data-stream.binance.vision";
 
